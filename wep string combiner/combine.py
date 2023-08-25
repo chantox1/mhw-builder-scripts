bbc3a7685c21b58f355213a65678ccd722878eeb
chantox <danielzegers22o@gmail.com>
2023-08-25 19:04:59 -0400

    Update combine.py

--- wep string combiner/combine.py
+++ wep string combiner/combine.py
@@ -13,14 +13,21 @@ weps = {
     'SA': 8,
     'CB': 9,
     'IG': 10,
+    'Bow': 11,
+    'HBG': 12,
+    'LBG': 13
 }
 
 input_dir = 'in'
 output_dir = 'out'
 
-f_data = os.path.join(input_dir, 'weps.json')
+f_data = os.path.join(input_dir, 'wepDataBM.json')
 with open(f_data) as f:
-    wep_data = json.load(f)
+    wep_data_bm = json.load(f)
+
+f_data = os.path.join(input_dir, 'wepDataGun.json')
+with open(f_data) as f:
+    wep_data_gun = json.load(f)
 
 _, dirs, _ = next(os.walk(input_dir))
 for lang in dirs:
@@ -36,8 +43,8 @@ for lang in dirs:
         wepId = weps[os.path.splitext(file)[0]]
 
         data[wepId] = {}
-        for _, val in wep_data[str(wepId)].items():
-            key = str(val['Name'])
+        for _, val in (wep_data_bm if wepId < 11 else wep_data_gun)[str(wepId)].items():
+            key = str(val['name'])
             if key in str_data:
                 data[wepId][key] = str_data[key]
     
@@ -48,7 +55,7 @@ for lang in dirs:
         if e.errno != errno.EEXIST:
             raise
 
-    f_out = os.path.join(output_dir_lang, 'wepStr.json')
+    f_out = os.path.join(output_dir_lang, 'wepNames.json')
     with open(f_out, 'w', encoding='utf-8') as f:
         f.write(json.dumps(data, ensure_ascii=False, separators=(',', ':')))
 
